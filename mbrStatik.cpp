bfca5fe02d35e5abc3cbdbba3a233d9320c897f7
halimbagusalhakim <halsat132@gmail.com>
2024-05-13 14:51:35 +0700

    mbrstatik

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -8,9 +8,12 @@ class mahasiswa {
         string nama;
 
         void setID();
+
+        void printAll();
         mahasiswa(string pnama):nama(pnama){
             setID();
         }
+        
         
 };
 
@@ -19,4 +22,22 @@ void mahasiswa::setID(){
     id = ++nim;
 }
 
+void mahasiswa::printAll(){
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+
+}
+
+int main(){
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+}
 
