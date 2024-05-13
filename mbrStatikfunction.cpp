e0e137434aa97e0be983657d75ce37a4923b0c76
halimbagusalhakim <halsat132@gmail.com>
2024-05-13 15:07:17 +0700

    membuat class mahasiswa

--- mbrStatikfunction.cpp
+++ mbrStatikfunction.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+    private:
+        static int nim;
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printAll();
+
+        static void setNim(int pNim) {nim = pNim;}
+        static int getNim() {return nim;}
+
+        Mahasiswa(string pnama):nama(pnama){
+            setID();
+        }
+};
